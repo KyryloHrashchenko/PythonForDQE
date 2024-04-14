aad026c2b4acf6a4a47df509de934b9d143ebb1f
EPAM\Kyrylo_Hrashchenko <Kyrylo_Hrashchenko@epam.com>
2024-04-14 23:34:30 +0300

    Homework 10

--- 10. Database API/Script_to_select_from_db.py
+++ 10. Database API/Script_to_select_from_db.py
@@ -5,7 +5,7 @@ connection = sqlite3.connect('test.db')
 cursor = connection.cursor()
 
 cursor.execute("SELECT * FROM private_ad")
-#connection.commit()
+connection.commit()
 result = cursor.fetchall()
 print(result)
 
